df3253ec44b1478ebb84c0b364b8d1e25e5d692e
adasu <61757201+amesysia@users.noreply.github.com>
2025-12-10 12:13:45 +0300

    Add files via upload

---
+++ hafta12/sayilar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h3>1-100 Arası Tek Sayılar</h3>
+    
+    <?php
+    for($i = 1; $i <= 100; $i += 2) {
+        echo $i . "  ";
+    }
+    ?>
+    
+</body>
+</html>
